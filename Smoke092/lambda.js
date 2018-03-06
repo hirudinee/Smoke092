0135a7779096dd9d9afa1071b1696caceeeddb70
hirudinee <35219641+hirudinee@users.noreply.github.com>
2018-03-06 10:08:36 +0530

    test

--- Smoke092/lambda.js
+++ Smoke092/lambda.js
@@ -1,7 +1,6 @@
 let AWS = require('aws-sdk');
 exports.handler = function(event, context, callback) {
 
-	//test comment
-	console.log('this is a sample console log.');
+
 	callback(null,'Successfully executed');
 }
